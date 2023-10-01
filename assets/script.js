32b70e29fea1608173d9b1b219b3fff5c41c64d1
bkness <kbrandon863@gmail.com>
2023-10-01 16:23:47 -0700

    got all of my forecasts to work once the new york button is pressed now im going to get the rest of my buttons woking and give my form section functionality

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Add an event listener to the New York button
-    document.getElementById('newYorkBtn').addEventListener('click', function () {
+      var searchInput = document.getElementById('focusedInput');
+        searchInput.addEventListener('click', function() {
+            this.value = '';
+        })
+
+        document.getElementById('newYorkBtn').addEventListener('click', function () {
         // Show the forecast containers
         document.querySelectorAll('.forecast-container, .col-md-2, .h3').forEach(function (element) {
             element.style.display = 'block';
@@ -37,7 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
             var firstDayData = data.list[0];
 
-            var date = firstDayData.dt_txt;
+            var date = new Date(firstDayData.dt_txt).toLocaleDateString();
             var temperature = firstDayData.main.temp;
             var windSpeed = firstDayData.wind.speed;
             var humidity = firstDayData.main.humidity;
@@ -45,6 +50,73 @@ document.addEventListener('DOMContentLoaded', function () {
             var dayOneElement = document.getElementById('dayOne');
             if (dayOneElement) {
                 dayOneElement.innerHTML = `
+                    <h3>${date}</h3>
+                    <p>Temperature: ${temperature}°F</p>
+                    <p>Wind Speed: ${windSpeed} mph</p>
+                    <p>Humidity: ${humidity}%</p>
+                `;
+            }
+
+            var secondDayData = data.list[8];
+
+            var date = new Date(secondDayData.dt_txt).toLocaleDateString();
+            var temperature = secondDayData.main.temp;
+            var windSpeed = secondDayData.wind.speed;
+            var humidity = secondDayData.main.humidity;
+
+            var dayTwoElement = document.getElementById('dayTwo');
+            if (dayTwoElement) {
+                dayTwoElement.innerHTML = `
+                    <h3>${date}</h3>
+                    <p>Temperature: ${temperature}°F</p>
+                    <p>Wind Speed: ${windSpeed} mph</p>
+                    <p>Humidity: ${humidity}%</p>
+                `;
+            }
+            
+            var thirdDayData = data.list[16];
+
+            var date = new Date(thirdDayData.dt_txt).toLocaleDateString();
+            var temperature = thirdDayData.main.temp;
+            var windSpeed = thirdDayData.wind.speed;
+            var humidity = thirdDayData.main.humidity;
+
+            var dayThreeElement = document.getElementById('dayThree');
+            if (dayThreeElement) {
+                dayThreeElement.innerHTML = `
+                    <h3>${date}</h3>
+                    <p>Temperature: ${temperature}°F</p>
+                    <p>Wind Speed: ${windSpeed} mph</p>
+                    <p>Humidity: ${humidity}%</p>
+                `;
+            }
+            
+            var fourthDayData = data.list[24];
+
+            var date = new Date(fourthDayData.dt_txt).toLocaleDateString();
+            var temperature = fourthDayData.main.temp;
+            var windSpeed = fourthDayData.wind.speed;
+            var humidity = fourthDayData.main.humidity;
+
+            var dayFourElement = document.getElementById('dayFour');
+            if (dayFourElement) {
+                dayFourElement.innerHTML = `
+                    <h3>${date}</h3>
+                    <p>Temperature: ${temperature}°F</p>
+                    <p>Wind Speed: ${windSpeed} mph</p>
+                    <p>Humidity: ${humidity}%</p>
+                `;
+            }
+            var fifthDayData = data.list[32];
+
+            var date = new Date(fifthDayData.dt_txt).toLocaleDateString();
+            var temperature = fifthDayData.main.temp;
+            var windSpeed = fifthDayData.wind.speed;
+            var humidity = fifthDayData.main.humidity;
+
+            var dayFiveElement = document.getElementById('dayFive');
+            if (dayFiveElement) {
+                dayFiveElement.innerHTML = `
                     <h3>${date}</h3>
                     <p>Temperature: ${temperature}°F</p>
                     <p>Wind Speed: ${windSpeed} mph</p>
